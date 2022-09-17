9e7e21024bfd1427302dc2948285d4c1b8ee3929
johan300 <johanandresrojasnaranjo@gmail.com>
2022-09-17 02:21:16 -0500

    le falta el modulo de enpresa y el registro

---
+++ bd/bd.js
@@ -0,0 +1,10 @@
+// Nombres y apellidos
+// Tipo de documento (mínimo 3 en un combobox) 
+// Documento
+// Email
+// Teléfono
+// Dirección
+// Contraseña
+// Confirmar contraseña
+// Botón de registrar
+
---
+++ js/taller.js
@@ -0,0 +1,12 @@
+
+    
+
+ 
+
+const velocidad=(D,T)=>{
+
+    let D=prompt(`Kilometro`);
+    let T=prompt(`Tienpo`);
+    let resultado= (D/T)/3.6;
+    document.write(resultado);
+}
